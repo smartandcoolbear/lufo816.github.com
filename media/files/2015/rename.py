40178c8addfed07e4f1d8f99d4ba0f6ff5ed44f8
lufo <lufo816@gmail.com>
2015-09-18 10:38:14 +0800

    new post

--- media/files/2015/rename.py
+++
@@ -1,9 +0,0 @@
-import os
-
-i = 1
-path = '.'
-for file in os.listdir(path):
-	if os.path.isfile(os.path.join(path,file))==True:
-		if file.find('jpg')>=0:
-			os.rename(os.path.join(path,file),os.path.join(path,"{:0>2d}".format(i)+'.jpg'))
-			i+=1
